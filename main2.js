6d339b17d325cd3d26904c755576df12e5110fb0
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-11 15:31:51 +0100

    feat: Implemented programiz 67

---
+++ main2.js
@@ -0,0 +1,11 @@
+// JavaScript Program to Check if An Array Contains a Specified Value
+
+const array = ['you', 'will', 'learn', 'javascript'];
+
+const hasValue = array.includes('javascript');
+
+if(hasValue) {
+    console.log('Array contains a value.')
+} else {
+    console.log('Array does not contain a value.')
+}
