8d3ed76844ccc166488dc62fb2ec53662a789466
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-11 15:33:26 +0100

    feat: Implemented programiz 67

--- main2.js
+++ main2.js
@@ -2,7 +2,7 @@
 
 const array = ['you', 'will', 'learn', 'javascript'];
 
-const hasValue = array.includes('javascript');
+const hasValue = array.indexOf('javascript') !== -1;
 
 if(hasValue) {
     console.log('Array contains a value.')
