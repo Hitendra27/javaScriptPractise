a7387670b93675748503e3a103e2bf11af3a1fe2
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-12 20:37:26 +0400

    feat: Implemented JS Programiz 76 b

--- main2.js
+++ main2.js
@@ -1,25 +1,11 @@
 // JavaScript Program to Sort Array of Objects by Property Values
 
-function compareName(a, b) {
-
-    const name1 = a.name.toUpperCase();
-    const name2 = b.name.toUpperCase();
-
-    let comparison = 0;
-
-    if(name1 > name2) {
-        comparison = 1;
-    } else if (name1 < name2) {
-        comparison = -1;
-    }
-    return comparison;
-
-}
+const compareAge = (a, b) => a.age - b.age;
 
 const students = [
     {
         name: 'Sara',
-        age: 24
+        age: 22
     },
     {
         name: 'John',
@@ -31,4 +17,4 @@ const students = [
     }
 ];
 
-console.log(students.sort(compareName));
+console.log(students.sort(compareAge));
