b0d309c62bb6d32682bd07bca7ab20a4a6325f8c
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-31 18:29:50 +0400

    feat: Implemented JS Programiz 90

--- main2.js
+++ main2.js
@@ -1,7 +1,16 @@
-// JavaScript Program To Get The Current URL
+// JavaScript Program to Validate An Email Address
 
-const url1 = window.location.href;
-const url2 = document.URL;
+function validateEmail(email_id) {
+  const regex_pattern =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-console.log(url1);
-console.log(url2);
+  if (regex_pattern.test(email_id)) {
+    console.log("The email address is valid");
+  } else {
+    console.log("The email address is not valid");
+  }
+}
+
+validateEmail("abc123@gmail.com");
+validateEmail("hello@com");
+validateEmail("sanjay_quessou@yahoo.co.uk");
