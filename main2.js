ae09d5f1b6095c6599fcf68e67a1e1a382a4dc18
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-31 18:40:24 +0400

    feat: Implemented JS Programiz 92c

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 // JavaScript Program to Check If a Variable is of Function Type
 
 function testVariable(variable) {
-  if (Object.prototype.toString.call(variable) == "[object Function]") {
+  if (typeof variable === "function") {
     console.log("The variable is of function type");
   } else {
     console.log("The variable is not of function type");
