a9d87f491eeb72e88a592e350e469af5778861f1
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-01 14:15:02 +0400

    feat: Implement JS Programiz 93b

--- main2.js
+++ main2.js
@@ -1,8 +1,9 @@
 // JavaScript Program to Pass Parameter to a setTimeout() Function
 
-const greet = () => {
-  console.log('Hello world');
-}
+const greet = (x, y) => {
+  console.log(x);
+  console.log(y);
+};
 
-setTimeout(greet, 3000);
-console.log('This message is shown first');
+setTimeout(greet, 3000, "hello", "world");
+console.log("This message is shown first");
