ccfb22267f2a3a67dce62ef222d853f9718671b5
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-29 21:58:31 +0400

    feat: Implemented JS Programiz 5b

--- main2.js
+++ main2.js
@@ -1,13 +1,9 @@
 // JavaScript Program to Swap Two Variables
 
-let a = prompt('Enter the first variable: ');
-let b = prompt('Enter the second variable: ');
+let a = prompt("Enter the first variable: ");
+let b = prompt("Enter the second variable: ");
 
-let temp;
-
-temp = a;
-a = b;
-b = temp;
+[a, b] = [b, a];
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
