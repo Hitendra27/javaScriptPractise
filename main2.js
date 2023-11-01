c59e6ca2faf050db8662ed68a38772192f34c017
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-11-01 21:57:16 +0000

    feat: Implemented JS Programiz 4b

--- main2.js
+++ main2.js
@@ -1,8 +1,11 @@
 // JavaScript Program to Calculate the Area of a Triangle
 
-const baseValue = prompt("Enter the base of a triangel: ");
-const heightValue = prompt("Enter the height of a triangle: ");
+const side1 = parseInt(prompt("Enter side1: "));
+const side2 = parseInt(prompt("Enter side2: "));
+const side3 = parseInt(prompt("Enter side3: "));
 
-const areaValue = (baseValue * heightValue) / 2;
+const s = (side1 + side2 + side3) / 2;
 
-console.log(`The are of the triangle is ${areaValue}`);
+const areaValue = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+
+console.log(`The area of the triangle is ${areaValue}`);
