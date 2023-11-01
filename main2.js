1482bfa287374ad90212a3f5dde8e4bd6a484f32
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-11-01 21:48:19 +0000

    feat: Implemented JS Programiz 3b

--- main2.js
+++ main2.js
@@ -1,7 +1,13 @@
 // JavaScript Program to Find the Square Root
 
-const number = prompt('Enter the number: ');
+const number1 = 2.25;
+const number2 = -4;
+const number3 = "hello";
 
-const result = Math.sqrt(number);
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
 
-console.log(`The square root of ${number} is ${result}`);
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
