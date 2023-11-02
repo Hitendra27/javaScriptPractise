f6504d2419ec2335c800dc2a29c0bc9ef789683e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-11-02 21:54:46 +0000

    feat: Implemented JS Programiz 5b

--- main2.js
+++ main2.js
@@ -3,11 +3,7 @@
 let a = prompt("Enter the first variable: ");
 let b = prompt("Enter the second variable: ");
 
-let temp;
-
-temp = a;
-a = b;
-b = temp;
+[a, b] = [b, a];
 
 console.log(`The value of a after swapping is ${a}`);
 console.log(`The value of b after swapping is ${b}`);
