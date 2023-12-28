9ae8f79313262ce1708ebb29adae37473558c9ae
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-28 20:17:47 +0000

    feat: Implemented JS programiz 10a

--- main2.js
+++ main2.js
@@ -1,5 +1,11 @@
-// Javascript Program to Generate a Random Number
+// Javascript Program to Check if a number is Positive, Negative, or Zero
 
-const a = Math.random();
+const number = 9;
 
-console.log(a);
+if (number > 0) {
+  console.log("The number is positive");
+} else if (number == 0) {
+  console.log("The number is zero");
+} else {
+  console.log("The number is negative");
+}
