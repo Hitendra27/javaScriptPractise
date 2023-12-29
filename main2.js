2bc85af51d6fee1e812809d6920be6faec5ad285
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-29 19:37:42 +0000

    feat: Implemented JS programiz 12b

--- main2.js
+++ main2.js
@@ -1,16 +1,9 @@
 // JavaScript Program to Find the Largest Among Three Numbers
 
-const num1 = 45;
+const num1 = 96;
 const num2 = 88;
 const num3 = 77;
-let Largest;
 
-if (num1 >= num2 && num1 >= num3) {
-  Largest = num1;
-} else if (num2 >= num1 && num2 >= num3) {
-  Largest = num2;
-} else {
-  Largest = num3;
-}
+let largest = Math.max(num1, num2, num3)
 
-console.log(`The largest number is: ${Largest}`);
+console.log(`The largest number is: ${largest}`);
