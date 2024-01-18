c20f9ec848eb907b915bf91b2903175390cbce68
Hitendra Quessou <73651340+Hitendra27@users.noreply.github.com>
2024-01-18 12:21:56 +0000

    Update main2.js

--- main2.js
+++ main2.js
@@ -6,3 +6,9 @@ const animals = ["dog", "cat", "lion", "tiger", "rabbit"];
 for (let i = 0; i < animals.length; i++) {
     console.log(animals[i].toUpperCase());
 }
+
+// for Each
+
+const transport = ["car", "bike", "tube", "overground", "national rail", "buses"];
+
+transport.forEach(a => console.log(a.toUpperCase());
