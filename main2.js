822cefe016cd538279efc53dbeba45701260c9ab
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-18 22:23:41 +0000

    feat: Implemented js programiz 12

--- main2.js
+++ main2.js
@@ -4,6 +4,6 @@ const num1 = 96;
 const num2 = 88;
 const num3 = -53;
 
-let largest = Math.min(num1, num2, num3)
+let largest = Math.min(num1, num2, num3);
 
 console.log(`The largest number is: ${largest}`);
