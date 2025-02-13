b5f38fce638f3c898d2d715988a2209b65759ff4
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-02-13 13:32:20 +0000

    feat: js programiz 2a

--- main4.js
+++ main4.js
@@ -1,2 +1,7 @@
-// the hello world program
-document.write('Hello World');
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
