e79c79291f36a4b460ed8dd37e8411677193cf96
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-04-06 22:05:23 +0100

    feat: implemented capitalize challenge

--- main4.js
+++ main4.js
@@ -1,7 +1,28 @@
-const num1 = 5;
-const num2 = 3;
+// Capitalize Challenge
+// Instructions:
 
-// add two numbers
-const sum = num1 + num2;
+// Take the variable myString and capitalize the first letter of the word using some of the methods that we talked about in the last video. Put the result in a variable called myNewString.
 
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
+// Create multiple solutions if you would like.
+
+// Expected Result:
+
+// const myString = 'developer';
+
+// console.log(myNewString); // 'Developer'
+// Hints:
+
+// You can use the charAt() method as well as string[index] to get the character at a specific index.
+// The .toUpperCase() method will make the entire string uppercase
+// substring() or slice() will return a specific portion of a string
+
+
+
+
+let myNewString;
+
+const myString = "developer";
+
+myNewString = `${myString.charAt(0).toUpperCase()}${myString.slice(1)}`;
+
+console.log(myNewString);
