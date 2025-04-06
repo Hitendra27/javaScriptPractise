df9d852b4ea583634b8c128cfe6628fcba4a78af
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-04-06 22:55:38 +0100

    feat: implemented the Number Challenge from Traversy Media

--- main4.js
+++ main4.js
@@ -1,28 +1,51 @@
-// Capitalize Challenge
+// Number Challenge
 // Instructions:
 
-// Take the variable myString and capitalize the first letter of the word using some of the methods that we talked about in the last video. Put the result in a variable called myNewString.
+// Create a variable called x that is a random number between 1 and 100 along with a variable called y that is a random number between 1 and 50.
 
-// Create multiple solutions if you would like.
+// Create a variable for the sum, difference, product, quotient and remainder of x and y. Log the output in a string that shows the two numbers of x and y along with the operator and result.
 
+// You can log the output string directly or put them in separate variables and log them like below.
+// You can use string concatenation or template literals for the output.
 // Expected Result:
 
-// const myString = 'developer';
-
-// console.log(myNewString); // 'Developer'
+// console.log(sumOutput); // 31 + 15 = 46
+// console.log(differenceOutput); // 31 - 15 = 16
+// console.log(productOutput); // 31 * 15 = 465
+// console.log(quotientOutput); // 31 / 15 = 2.066666666666667
+// console.log(rmOutput); // 31 % 15 = 1
 // Hints:
 
-// You can use the charAt() method as well as string[index] to get the character at a specific index.
-// The .toUpperCase() method will make the entire string uppercase
-// substring() or slice() will return a specific portion of a string
+// The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1
 
+// The Math.floor() function will round a number down to the nearest integer
 
+let x = Math.floor(Math.random() * 100) + 1;
 
+let y = Math.floor(Math.random() * 50) + 1;
 
-let myNewString;
+// sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
 
-const myString = "developer";
+//cdifference
+const difference = x - y;
+const differenceOutput = `${x} - ${y} = ${difference}`;
 
-myNewString = `${myString.charAt(0).toUpperCase()}${myString.slice(1)}`;
+// product
+const product = x * y;
+const productOutput = `${x} * ${y} = ${product}`;
 
-console.log(myNewString);
+// quotient
+const quotient = x / y;
+const quotientOutput = `${x} / ${y} = ${quotient}`;
+
+// rm
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+
+console.log(sumOutput);
+console.log(differenceOutput);
+console.log(productOutput);
+console.log(quotientOutput);
+console.log(rmOutput);
