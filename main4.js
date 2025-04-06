6fdf87ef8f83f9a78914e9fdf1481374fd4d073c
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-04-06 23:19:46 +0100

    feat: implemented array challenges with Traversy Media

--- main4.js
+++ main4.js
@@ -1,20 +1,26 @@
-// Array Challenges
-// Challenge 1:
+// Challenge 2:
 // Instructions:
 
-// Use some of the array methods that we looked at to mutate the following array to = the expected result below:
+// Combine arr1 and arr2 into a new array called arr3 with the following elements:
 
-// const arr = [1, 2, 3, 4, 5];
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [5, 6, 7, 8, 9, 10];
+// Notice that both arr1 and arr2 include the number 5. You will have to find a way to get rid of the extra 5.
+
 // Expected Result:
 
-// console.log(arr);
-// // [6, 5, 4, 3, 2, 1, 0];
-// Hint: No hints. This one is pretty easy ;)
+// console.log(arr3);
+// // [1,2,3,4,5,6,7,8,9,10]
+// Hint: There are many ways to do this, but think of the concat() method or the spread operator as well as the slice() or splice() methods
 
-const arr = [1, 2, 3, 4, 5];
+const arr1 = [1, 2, 3, 4, 5];
 
-arr.reverse().unshift(6);
+const arr2 = [5, 6, 7, 8, 9, 10];
 
-arr.push(0);
+//const arr3 = arr1.concat(arr2);
 
-console.log(arr);
+const arr3 = [...arr1, ...arr2];
+
+arr3.splice(4, 1);
+
+console.log(arr3);
