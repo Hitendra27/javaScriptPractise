0e968da82b42eaea9376804f585822da384fcc7f
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-04-10 20:34:41 +0100

    feat: implemented higher order challenge with Travesy Media

--- main4.js
+++ main4.js
@@ -1,49 +1,5 @@
-// Higher Order Challenges with Traversy Media
-const people = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    email: "john@gmail.com",
-    phone: "111-111-1111",
-    age: 30,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Poe",
-    email: "jane@gmail.com",
-    phone: "222-222-2222",
-    age: 25,
-  },
-  {
-    firstName: "Bob",
-    lastName: "Foe",
-    email: "bob@gmail.com",
-    phone: "333-333-3333",
-    age: 45,
-  },
-  {
-    firstName: "Sara",
-    lastName: "Soe",
-    email: "Sara@gmail.com",
-    phone: "444-444-4444",
-    age: 19,
-  },
-  {
-    firstName: "Jose",
-    lastName: "Koe",
-    email: "jose@gmail.com",
-    phone: "555-555-5555",
-    age: 23,
-  },
-];
+const numbers = [2, -30, 50, 20, -12, -9, 7];
 
-const youngPeople = people
-  .filter((a) => a.age <= 25)
-  .map((b) => {
-    return {
-      name: `${b.firstName} ${b.lastName}`,
-      email: b.email,
-    };
-  });
+const positiveSum = numbers.filter((a) => a >= 0).reduce((a, b) => a + b, 0);
 
-console.log(youngPeople);
+console.log(positiveSum);
